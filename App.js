24b37f17dfbf3e40a67ece22937ba3e5b294291c
Togami Yuki <togaminnnn@gmail.com>
2020-03-14 14:52:31 +0900

    no message

---
+++ App.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import { HomeScreen } from './screen/HomeScreen';
+import { DetailScreen } from './screen/DetailScreen';
+
+const Stack = createStackNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator
+        initialRouteName="Home"
+        screenOptions={{
+          headerTintColor:"#ffffff",
+          headerStyle: {
+            backgroundColor: "#333399",
+          }
+        }}
+        headerMode="float"
+      >
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen
+          name="Detail" component={DetailScreen}
+          options={{
+          }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
